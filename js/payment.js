d2d2af52732955229bbb5714a82ac32d0f390a87
Ibiral <tnt_djurdjura@hotmail.fr>
2021-01-13 13:31:40 +0100

    validation du formulaire en js

--- js/payment.js
+++ js/payment.js
@@ -41,3 +41,28 @@ function remove(id){
     showCart(dataManager.products);
 }
 
+
+document.forms["signIn"].addEventListener("submit", function(e) {
+ 
+	var erreur;
+ 
+	var inputs = this;
+ 
+	// Traitement générique
+	for (var i = 0; i < inputs.length; i++) {
+		console.log(inputs[i]);
+		if (!inputs[i].value) {
+			erreur = "Veuillez renseigner tous les champs";
+			break;
+		}
+    }
+    
+    if (erreur) {
+		e.preventDefault();
+		document.getElementById("erreur").innerHTML = erreur;
+		return false;
+	} else {
+		alert('Formulaire envoyé !');
+	}
+	
+});
