598c0731ada33f1efe2b37893b8821195cde1a23
Ibiral <tnt_djurdjura@hotmail.fr>
2021-02-23 10:49:32 +0100

    test serveur en ligne

--- js/common.js
+++ js/common.js
@@ -1,2 +1,2 @@
-const dataManager = new DataManager("http://localhost:3000/api/cameras/");
+const dataManager = new DataManager("https://orinocamera.herokuapp.com/api/cameras");
 const cart = new Basket();
